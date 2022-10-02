638867e698cbefc8254e3efdf20a2cc99d639986
Ajay Paralkar <61840062+ajay21-hub@users.noreply.github.com>
2022-10-02 21:36:05 +0530

    Add files via upload

---
+++ StartSudokuSolver.java
@@ -0,0 +1,11 @@
+import Sudoku.SudokuFrame;
+
+public class StartSudokuSolver {
+    public static void main(String[] args) {
+        openGUI();
+    }
+
+    public static void openGUI(){
+        new SudokuFrame();
+    }
+}
